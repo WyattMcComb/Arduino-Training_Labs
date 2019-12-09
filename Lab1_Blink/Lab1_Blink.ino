a03f3a0dcd24c586407ac332b9d8826673e9ae43
Wyatt McComb <02wyatt.mccomb@gulllakecs.org>
2019-12-09 10:39:32 -0800

    Potentiometer working from 0 - 1023

--- Lab1_Blink/Lab1_Blink.ino
+++ Lab1_Blink/Lab1_Blink.ino
@@ -13,8 +13,18 @@
 
 
 void setup() {
+  // initialize the serial communication at 9600 bits per second
+  Serial.begin(9600);
   
 }
 
 void loop() {
+  // read the input on analog pin 0
+  int potentiometerValue = analogRead(A0);
+
+  // print the value of our potnetiometer
+  Serial.println(potentiometerValue);
+
+  // Add a delay for stability
+  delay(1);
 }
